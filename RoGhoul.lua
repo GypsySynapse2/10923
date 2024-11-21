033250a30382407f89713b34aef7424867cd4ed8
Gypsy <130881047+GypsySynapse2@users.noreply.github.com>
2024-11-21 18:40:11 +0000

    Update RoGhoul.lua

--- RoGhoul.lua
+++ RoGhoul.lua
@@ -445,7 +445,7 @@ end
 
 local GC = getconnections or get_signal_cons
 	if GC then
-		for i,v in pairs(GC(Players.LocalPlayer.Idled)) do
+		for i,v in pairs(GC(game.Players.LocalPlayer.Idled)) do
 			if v["Disable"] then
 				v["Disable"](v)
 			elseif v["Disconnect"] then
